dd82500a04cd9503e1aa86d1a52674a3721543eb
Janxyxy <jankroupa2@gmail.com>
2025-01-27 13:58:53 +0100

    Finaly

--- Assets/Scripts_beyblade/DefaultPart.cs
+++ Assets/Scripts_beyblade/DefaultPart.cs
@@ -1,57 +1,9 @@
+
 using UnityEngine;
 
 public class DefaultPart : Part
 {
-    // Fields for private storage
-    private int hp = 20;
-    private float speed = 2;
-    private float rotationTime = 10;
-    private float knockbackForce = 10;
-    private Material skin;
-    private Sprite icon;
-
-    // Override part attributes
-    public override string PartName => "Default Part";
-    public override PartRarity Rarity => PartRarity.Common;
-    public override float PassiveDamage => 0;
-    public override Material Skin
-    {
-        get => skin;
-        set => skin = value;
-    }
-
-    public override Ability Ability => new DefaultAbility();
-
-    public override int Hp
-    {
-        get => hp;
-        set => hp = Mathf.Max(0, value); // Ensure HP is non-negative
-    }
-
-    public override float Speed
-    {
-        get => speed;
-        set => speed = Mathf.Max(0, value); // Ensure speed is non-negative
-    }
-
-    public override float RotationTime
-    {
-        get => rotationTime;
-        set => rotationTime = Mathf.Max(0, value); // Ensure rotation time is non-negative
-    }
-
-    public override float KnockbackForce
-    {
-        get => knockbackForce;
-        set => knockbackForce = Mathf.Max(0, value); // Ensure knockback force is non-negative
-    }
-
-    // Define the type of part explicitly
-    public override PartType Type => PartType.Disk;
-
-    public override Sprite Icon
-    {
-        get => icon;
-        //set => icon = value;
-    }
+ 
 }
+
+
--- Assets/Scripts_beyblade/Part.cs
+++ Assets/Scripts_beyblade/Part.cs
@@ -1,46 +1,53 @@
 using UnityEngine;
 
-public abstract class Part
+public abstract class Part : MonoBehaviour
 {
-    // Unique identifier for the part
-    public abstract string PartName { get; }
+    // Reference to the PartSO
+    [SerializeField] private PartSO partSO;
 
-    // Rarity of the part
-    public abstract PartRarity Rarity { get; }
+    // Override part attributes from PartSO
+    public virtual string PartName => partSO.partName;
+    public virtual PartSO.PartRarity Rarity => partSO.rarity;
+    public virtual float PassiveDamage => partSO.passiveDamage;
 
-    // Passive damage, 0 if not applicable
-    public abstract float PassiveDamage { get; }
-
-    // Material or appearance of the part
-    public abstract Material Skin { get; set; }
-
-    public abstract PartType Type { get; }
-
-    // Enum for the type of part
-    public enum PartType
+    public virtual Material Skin
     {
-        Disk,
-        Ratchet,
-        Bit
+        get => partSO.skin;
+        set => partSO.skin = value; // This might modify the PartSO asset
     }
 
-    // Enum for rarity of the part
-    public enum PartRarity
+    public virtual Ability Ability => new DefaultAbility();
+
+    public virtual int Hp
     {
-        Common,
-        Rare,
-        Epic,
-        Legendary
+        get => partSO.hp;
+        set => partSO.hp = Mathf.Max(0, value); // Ensure HP is non-negative
     }
 
-    // Ability associated with the part
-    public abstract Ability Ability { get; }
+    public virtual float Speed
+    {
+        get => partSO.speed;
+        set => partSO.speed = Mathf.Max(0, value); // Ensure speed is non-negative
+    }
 
-    // Attributes of the part
-    public abstract int Hp { get; set; }
-    public abstract float Speed { get; set; }
-    public abstract float RotationTime { get; set; }
-    public abstract float KnockbackForce { get; set; }
+    public virtual float RotationTime
+    {
+        get => partSO.rotationTime;
+        set => partSO.rotationTime = Mathf.Max(0, value); // Ensure rotation time is non-negative
+    }
 
-    public abstract Sprite Icon { get; }
+    public virtual float KnockbackForce
+    {
+        get => partSO.knockbackForce;
+        set => partSO.knockbackForce = Mathf.Max(0, value); // Ensure knockback force is non-negative
+    }
+
+    // Define the type of part explicitly from PartSO
+    public virtual PartSO.PartType Type => partSO.type;
+
+    public virtual Sprite Icon
+    {
+        get => partSO.icon;
+        // set => partSO.icon = value; // If you want to modify icon in runtime
+    }
 }
