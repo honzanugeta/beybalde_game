210de68fff3832a67a7adb55faaaa16a41555495
Janxyxy <jankroupa2@gmail.com>
2025-01-28 13:46:32 +0100

    Inventory

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using VInspector;
 
 public class Inventory : MonoBehaviour
 {
+    [Header("UI Storage")]
     [SerializeField] private Transform bladesGrid;
     [SerializeField] private Transform coresGrid;
     [SerializeField] private Transform rachetsGrid;
 
+    [Header("Selected Storage UI")]
+    [SerializeField] private Image selectedBladeUI;
+    [SerializeField] private Image selectedCoreUI;
+    [SerializeField] private Image selectedRachetUI;
+
+    [Header("Selected Storage")]
+    [SerializeField, ReadOnly] private PartSO selectedBlade;
+    [SerializeField, ReadOnly] private PartSO selectedCore;
+    [SerializeField, ReadOnly] private PartSO selectedRachet;
+
     [SerializeField] private PartUI partUiPrefab;
 
     [SerializeField] private SerializedDictionary<PartSO.PartRarity, Color> rarityColors;
+
+    private Dictionary<PartSO, PartUI> partUIDict = new Dictionary<PartSO, PartUI>();
 
     private PartsStorage partsStorage;
 
@@ -40,10 +56,55 @@ public class Inventory : MonoBehaviour
                     break;
             }
 
-            partUI.SetIcon(part.icon);
+            partUIDict.Add(part, partUI); // Add to dictionary
+            partUI.SetInventory(this);
 
+            partUI.SetIcon(part.icon);
             Color bgColor = rarityColors[part.rarity];
             partUI.SetRarity(bgColor);
         }
+    }
+
+    internal void SelectPart(PartUI partUI)
+    {
+        for (int i = 0; i < partsStorage.PartList.Count; i++)
+        {
+            if (partUIDict[partsStorage.PartList[i]] == partUI)
+            {
+                UpdateSelectedPart(partsStorage.PartList[i]);
+                break;
+            }
+        }
+    }
+
+    private void UpdateSelectedPart(PartSO part)
+    {
+        Image image = null;
+
+        switch (part.type)
+        {
+            case PartSO.PartType.Disk:
+                selectedBlade = part;
+                UpdateUI(selectedBladeUI, selectedBlade.icon, selectedBlade.rarity);
+                break;
+
+            case PartSO.PartType.Ratchet:
+                selectedRachet = part;
+                UpdateUI(selectedRachetUI, selectedRachet.icon, selectedRachet.rarity);
+                break;
+
+            case PartSO.PartType.Bit:
+                selectedCore = part;
+                UpdateUI(selectedCoreUI, selectedCore.icon, selectedCore.rarity);
+                break;
+        }
+    }
+
+    private void UpdateUI(Image uiElement, Sprite icon, PartSO.PartRarity rarity)
+    {
+        uiElement.color = rarityColors[rarity];
+        Image image = uiElement.GetComponentsInChildren<Image>(true)[1];
+        Debug.Log(image.name);
+        image.sprite = icon;
     }
 }
--- Assets/Scripts/Inventory/PartUI.cs
+++ Assets/Scripts/Inventory/PartUI.cs
@@ -1,20 +1,37 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PartUI : MonoBehaviour
 {
     [SerializeField] private Image icon;
+
+    private Button button;
+    private Inventory inventory;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnButtonClick);
+    }
     internal void SetIcon(Sprite newIcon)
     {
         icon.sprite = newIcon;
+    }
+
+    internal void SetInventory(Inventory newInventory)
+    {
+        inventory = newInventory;
     }
 
     internal void SetRarity(Color newColor)
     {
         Image bg = GetComponent<Image>();
         bg.color = newColor;
+    }
+
+    private void OnButtonClick()
+    {
+        inventory.SelectPart(this);
     }
 }
