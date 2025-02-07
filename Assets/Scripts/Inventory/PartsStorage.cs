a3912bfc9353a4f0cf41959d2b135aa10de55c5c
Janxyxy <jankroupa2@gmail.com>
2025-02-07 11:03:19 +0100

    Part Storage Rework

--- Assets/Scripts/Inventory/PartsStorage.cs
+++ Assets/Scripts/Inventory/PartsStorage.cs
@@ -3,8 +3,27 @@ using UnityEngine;
 
 public class PartsStorage : MonoBehaviour
 {
-    [SerializeField]private List<PartSO> partList = new List<PartSO>();
+    [SerializeField] private List<PartSO> partList = new List<PartSO>();
     internal List<PartSO> PartList => partList;
 
+    private void Awake()
+    {
+        LoadParts();
+    }
 
+    private void LoadParts()
+    {
+        // Load all PartSO from "Parts" and its subfolders
+        PartSO[] loadedParts = Resources.LoadAll<PartSO>("Parts");
+
+        if (loadedParts.Length > 0)
+        {
+            partList.AddRange(loadedParts);
+            Debug.Log($"Loaded {loadedParts.Length} parts.");
+        }
+        else
+        {
+            Debug.LogWarning("No parts found in Resources/Parts/.");
+        }
+    }
 }
