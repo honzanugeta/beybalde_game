89a884bc0a0dca94c04abcc06e50a6c9e55dd7e4
Janxyxy <jankroupa2@gmail.com>
2025-03-21 21:23:27 +0100

    Inventory + DataManger

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,8 @@ public class LobbyManager : MonoBehaviour
     public static LobbyManager instance;
 
     private Inventory inventory;
+    private PlayerCurrency playerCurrency;
+    private PartsStorage partsStorage;
 
     private void Awake()
     {
@@ -20,6 +23,25 @@ public class LobbyManager : MonoBehaviour
         }
 
         inventory = FindAnyObjectByType<Inventory>();
+        playerCurrency = FindAnyObjectByType<PlayerCurrency>();
+        partsStorage = FindAnyObjectByType<PartsStorage>();
+    }
+
+    private void Start()
+    {
+        playerCurrency.LoadCoins();
+
+        // Testing method to lock all parts
+        // LockAllParts();
+    }
+
+    private void LockAllParts()
+    {
+        Debug.Log($"Locking all parts... {partsStorage.PartList.Count}");
+        foreach (PartSO part in partsStorage.PartList)
+        {
+            DataManager.instance.LockPart(part.partName);
+        }
     }
 
     internal void OpenInventory(bool open)
@@ -27,5 +49,8 @@ public class LobbyManager : MonoBehaviour
         inventory.CanvasOpen(open);
     }
 
-
+    internal void RemoveCoins(int v)
+    {
+        throw new NotImplementedException();
+    }
 }
